80da874d37d023f712ac51e89bf98309e4d6de6f
alexis bleuse <bleuse.alexis@gmail.com>
2022-07-28 18:19:02 +0200

    added login route

--- controllers/Account.js
+++ controllers/Account.js
@@ -1,37 +1,75 @@
 const AccountModel = require("../models/Account");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const account = {
-  createAccount(req, res) {
-    const accountForm = req.body;
+const saltRounds = 10;
 
-    if (!accountForm.name) return res.sendStatus(400);
-    if (!accountForm.surname) return res.sendStatus(400);
-    if (!accountForm.email) return res.sendStatus(400);
-    if (!accountForm.password) return res.sendStatus(400);
+async function createAccount(req, res) {
+  const accountForm = req.body;
 
-    AccountModel.find({ email: accountForm.email })
-      .then((result) => {
-        if (result.length !== 0) return res.sendStatus(409);
-        else {
-          AccountModel.create(accountForm)
-            .then(() => {
-              res.sendStatus(201);
-            })
-            .catch(() => res.sendStatus(500));
-        }
-      })
-      .catch(() => res.sendStatus(500));
-  },
+  if (!accountForm.name) return res.sendStatus(400);
+  if (!accountForm.surname) return res.sendStatus(400);
+  if (!accountForm.email) return res.sendStatus(400);
+  if (!accountForm.password) return res.sendStatus(400);
 
-  deleteAccount(req, res) {
-    AccountModel.deleteOne({ _id: req.body })
-      .then(() => {
-        res.sendStatus(200);
-      })
-      .catch(() => {
-        res.sendStatus(400);
-      });
-  },
-};
+  const lowEmail = accountForm.email.toLowerCase().trim();
+
+  const isExisting = await AccountModel.findOne({ email: lowEmail });
+
+  if (isExisting === null) {
+    try {
+      const hashedPassword = await bcrypt.hash(
+        accountForm.password,
+        saltRounds
+      );
+      const newAccount = {
+        name: accountForm.name,
+        surname: accountForm.surname,
+        password: hashedPassword,
+        email: lowEmail,
+      };
+      await AccountModel.create(newAccount);
+      res.status(204).send("User created");
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  } else {
+    res.status(500).send("User already exists");
+  }
+}
+
+async function login(req, res) {
+  const accountForm = req.body;
+
+  if (!accountForm.email) return res.sendStatus(400);
+  if (!accountForm.password) return res.sendStatus(400);
+
+  const lowEmail = accountForm.email.toLowerCase().trim();
+  const account = await AccountModel.findOne({ email: lowEmail });
+  if (account === null) {
+    return res.status(400).send("No User Found");
+  }
+
+  const isMatch = await bcrypt.compare(accountForm.password, account.password);
+
+  const token = jwt.sign({ id: account._id }, process.env.DB_SECRET);
+
+  if (isMatch) res.send({ jwt: token });
+  else {
+    return res.status(400).send("Incorrect Login");
+  }
+}
+
+async function deleteAccount(req, res) {
+  AccountModel.deleteOne({ _id: req.body })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+}
+
+const account = { createAccount, deleteAccount, login };
 
 module.exports = account;
--- routes/account.js
+++ routes/account.js
@@ -5,4 +5,6 @@ var router = express.Router();
 router.post("/", Account.createAccount);
 
 router.post("/delete", Account.deleteAccount);
+
+router.post("/login", Account.login);
 module.exports = router;
